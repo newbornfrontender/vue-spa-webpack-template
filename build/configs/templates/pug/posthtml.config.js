86f763155eb239ac7ba8169a5ee698b18adc03b1
Infant Frontender <newbornfrontender@gmail.com>
2018-05-29 16:38:08 +0300

    Added posthtml config files

---
+++ build/configs/templates/pug/posthtml.config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const development = env === 'development';
+const production = env === 'production';
+
+module.exports = ({
+  env, file, options,
+}) => ({
+  parser: false,
+
+  plugins: {
+    'posthtml-doctype': {
+      doctype: 'HTML 5',
+    },
+
+    'posthtml-bem': {
+      elemPrefix: '__',
+      modPrefix: '--',
+      modDlmtr: '--',
+    },
+
+    'posthtml-attrs-sorter': {
+      order: [
+        'class', 'id', 'name',
+        'data-.+', 'ng-.+', 'src',
+        'for', 'type', 'href',
+        'values', 'title', 'alt',
+        'role', 'aria-.+',
+        '$unknown$',
+      ],
+    },
+
+    'posthtml-beautify': production ? false : {
+      rules: {
+        indent: 2,
+      },
+    },
+  },
+});
