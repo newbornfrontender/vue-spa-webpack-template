7bbe2e307673d15278b45fab632b7291c6c451c1
Infant Frontender <newbornfrontender@gmail.com>
2018-06-08 15:37:28 +0300

    Created webpack part

---
+++ build/configs/parts/dev-server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// =============================================================================
+// = DEVSERVER                                                                 =
+// =============================================================================
+
+export default ({
+  host, port,
+} = {}) => ({
+  devServer: {
+    clientLogLevel: 'warning',
+    hot: true,
+    contentBase: 'dist', // ?
+    compress: true,
+    // stats: "errors-only",
+    host,
+    port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    publicPath: '/',
+    quiet: true,
+    watchOptions: {
+      poll: true,
+    },
+  },
+});
