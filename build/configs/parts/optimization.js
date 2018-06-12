799c25768f3d45dc102c0a1fec39abb6ac395de9
Infant Frontender <newbornfrontender@gmail.com>
2018-06-12 14:15:38 +0300

    Changed optimization options

--- build/configs/parts/optimization.js
+++ build/configs/parts/optimization.js
@@ -4,16 +4,37 @@
 // = WEBPACK PART CONFIG                                                       =
 // =============================================================================
 
+// export default ({
+//   name, filename,
+// } = {}) => ({
+//   optimization: {
+//     splitChunks: {
+//       cacheGroups: {
+//         commons: {
+//           test: /[\\/]node_modules[\\/]/,
+//           name,
+//           filename,
+//           chunks: 'all',
+//         },
+//       },
+//     },
+//   },
+// });
+
 export default ({
-  name,
-} = {}) => ({
   optimization: {
     splitChunks: {
+      chunks: 'all',
+      automaticNameDelimiter: '~',
+      name: true,
       cacheGroups: {
-        commons: {
+        vendors: {
           test: /[\\/]node_modules[\\/]/,
-          name,
-          chunks: 'all',
+          priority: -10,
+        },
+        default: {
+          priority: -20,
+          reuseExistingChunk: true,
         },
       },
     },
