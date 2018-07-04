25102994c438ece96f855c3a2859663bdb56904e
Infant Frontender <newbornfrontender@gmail.com>
2018-07-04 11:01:08 +0300

    Files moved or deleted

--- build/config/parts/loaders/postcss.js
+++
@@ -1,4 +0,0 @@
-// test: /\.(css|(p|post)css|less|s?[a|c]ss|sss)$/,
-// test: /\.((s?[a|c]|c)ss|(p|post)css|less|sss)$/,
-// test: /\.(c|le|s?[a|c]|(p|post)|(s|sugar))ss$/,
-// test: /\.(c|le|s?[a|c]|p(ost)?|s(ugar)?)ss$/,
--- config/processors/stylus/postcss.config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = ({ env, file, options }) => ({
-  plugins: {
-    'autoprefixer': env === 'production' ? options.autoprefixer : false,
-  },
-});
